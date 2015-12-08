56225900c82465faba3aac527b326b4351135e41
Garrett Dawson <killtheliterate@gmail.com>
2015-12-08 15:41:05 -0700

    Fix typos. Whoops.

--- src/connection.js
+++ src/connection.js
@@ -3,14 +3,14 @@ import Rx from 'rx'
 
 const fromEvent = Rx.Observable.fromEvent
 
-export default function observeableSocket (address) {
-    const _ws = new WebSocket(address)
+export default function observeableSocket (socketAddress) {
+    const _ws = new WebSocket(socketAddress)
 
     const openStream = fromEvent(_ws, 'open')
     const closeStream = fromEvent(_ws,'close')
 
     const messageStream = openStream
-        .flatMap(() => fromEvent(ws, 'message'))
+        .flatMap(() => fromEvent(_ws, 'message'))
         .takeUntil(closeStream)
 
     const address = new Promise(function(resolve) {
@@ -24,6 +24,6 @@ export default function observeableSocket (address) {
             address.then(proxy => proxy(message))
         },
 
-        signal: parseStream,
+        signal: messageStream,
     }
 }
