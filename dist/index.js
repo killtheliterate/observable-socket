3d161d633ce32727e4373c173434c3d18423de50
Garrett Dawson <killtheliterate@gmail.com>
2015-12-09 14:54:41 -0700

    Fix up project config

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _connection = require('./connection');
-
-var _connection2 = _interopRequireDefault(_connection);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-module.exports = _connection2.default;
