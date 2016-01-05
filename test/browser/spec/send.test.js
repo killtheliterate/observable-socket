86d4eaf9e01238955cfe5960a4cbb8aa34eb507a
Garrett Dawson <killtheliterate@gmail.com>
2016-01-04 23:10:30 -0700

    Reformat code a bit, get linting working

--- test/browser/spec/send.test.js
+++ test/browser/spec/send.test.js
@@ -4,20 +4,20 @@ var createConnection = require('../../../dist/index')
 
 describe('socket.send', function () {
 
-    describe('open socket', function() {
-        it('sends immediately if socket is already open', function(done) {
+    describe('open socket', function () {
+        it('sends immediately if socket is already open', function (done) {
             this.timeout(3000)
 
             var socket = new WebSocket('ws://echo.websocket.org')
 
-            setTimeout(function() {
+            setTimeout(function () {
                 const stream = createConnection(socket)
 
                 stream.send('old socket')
 
                 var sub = stream.observable.take(1)
 
-                sub.subscribe(function(el) {
+                sub.subscribe(function (el) {
                     expect(el).to.equal('old socket')
 
                     done()
@@ -26,15 +26,15 @@ describe('socket.send', function () {
         })
     })
 
-    describe('new socket', function() {
-        it('waits to send until socket is open', function(done) {
+    describe('new socket', function () {
+        it('waits to send until socket is open', function (done) {
             var stream = createConnection(new WebSocket('ws://echo.websocket.org'))
 
             stream.send('new socket')
 
             var sub = stream.observable.take(1)
 
-            sub.subscribe(function(el) {
+            sub.subscribe(function (el) {
                 expect(el).to.equal('new socket')
 
                 done()
