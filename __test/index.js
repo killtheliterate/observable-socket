a4d91ea86ed369a435d9cd1d66f70b5f7c74756f
Garrett Dawson <killtheliterate@gmail.com>
2017-07-06 14:41:36 -0600

    Add unbound operators for even smaller build options
    This provides bundles for users who would like to cherry-pick operators
    out of RxJS to use with observable-socket

--- __test/index.js
+++ __test/index.js
@@ -1,2 +1,3 @@
 require('./spec/up.test')
 require('./spec/down.test')
+// require('./spec/bind.test') // separate test - could delete cached module
---
+++ src/observable-only.js
@@ -0,0 +1,5 @@
+import { Observable } from 'rxjs/Observable'
+import 'rxjs/add/observable/fromEvent'
+import 'rxjs/add/operator/take'
+
+export default Observable
